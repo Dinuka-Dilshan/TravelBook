1b8ba246e46332b688000622839ecd13475d61e2
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-22 22:40:18 +0530

    backend - app.js

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,33 @@
-import express from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
+import express, { urlencoded } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import userRoutes from "./Routes/UserRoutes.js";
 
 const app = express();
+app.use(cors());
+app.use(express.json(urlencoded));
 
-app.get("/", (req, res, next) => {
-  res.json({
-    msg: "hello",
-  });
-});
+app.use("/user", userRoutes);
 
-app.listen(process.env.PORT, (err) => {
-  if (!err) {
-    console.log(`server is listening on port ${process.env.PORT}`);
-  } else {
+
+app.use((err,req,res,next)=>{
+  res.status(err.errorCode).json(err)
+})
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    app.listen(process.env.PORT || 5000, (err) => {
+      if (!err) {
+        console.log(`server is listening on port ${process.env.PORT}`);
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .catch((err) => {
     console.log(err);
-  }
-});
+  });
