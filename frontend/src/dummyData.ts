1035021c907bc5009d401d1ad53a823f779c90d8
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-22 22:38:05 +0530

    frontend -hotels

---
+++ frontend/src/dummyData.ts
@@ -0,0 +1,214 @@
+export const places = [
+  {
+    title: "Sigiriya",
+    description: `. Lorem Ipsum has been  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in`,
+    rating: 3,
+  },
+  {
+    title: "Sigiriya",
+    description: `. Lorem Ipsum has been  when an unknown printer took  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in`,
+    rating: 3,
+  },
+  {
+    title: "Sigiriya",
+    description: `. Lorem Ipsum has been  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in`,
+    rating: 3,
+  },
+  {
+    title: "Sigiriya",
+    description: `. Lorem Ipsum has been  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in`,
+    rating: 3,
+  },
+  {
+    title: "Sigiriya",
+    description: `. Lorem Ipsum has been  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in`,
+    rating: 3,
+  },
+  {
+    title: "Sigiriya",
+    description: `. Lorem Ipsum has been  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in`,
+    rating: 3,
+  },
+  {
+    title: "Sigiriya",
+    description: `. Lorem Ipsum has been  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in`,
+    rating: 3,
+  },
+  {
+    title: "Sigiriya",
+    description: `. Lorem Ipsum has been  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in`,
+    rating: 3,
+  },
+];
+
+export const placeImages = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+    cols: 2,
+  },
+];
+
+export const messages = [
+  {
+    image:
+      "https://i.pinimg.com/736x/6f/de/85/6fde85b86c86526af5e99ce85f57432e.jpg",
+    name: "Sayushi Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 20,
+    time:'2h ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/ab/ba/70/abba702dbba627fc3e7c05213b3f2ce6.jpg",
+    name: "Kamala Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 120,
+    time:'1 day ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/6f/de/85/6fde85b86c86526af5e99ce85f57432e.jpg",
+    name: "Sayushi Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 20,
+    time:'2h ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/ab/ba/70/abba702dbba627fc3e7c05213b3f2ce6.jpg",
+    name: "Kamala Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 120,
+    time:'1 day ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/6f/de/85/6fde85b86c86526af5e99ce85f57432e.jpg",
+    name: "Sayushi Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 20,
+    time:'2h ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/ab/ba/70/abba702dbba627fc3e7c05213b3f2ce6.jpg",
+    name: "Kamala Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 120,
+    time:'1 day ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/6f/de/85/6fde85b86c86526af5e99ce85f57432e.jpg",
+    name: "Sayushi Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 20,
+    time:'2h ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/ab/ba/70/abba702dbba627fc3e7c05213b3f2ce6.jpg",
+    name: "Kamala Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 120,
+    time:'1 day ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/6f/de/85/6fde85b86c86526af5e99ce85f57432e.jpg",
+    name: "Sayushi Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 20,
+    time:'2h ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/ab/ba/70/abba702dbba627fc3e7c05213b3f2ce6.jpg",
+    name: "Kamala Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 120,
+    time:'1 day ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/6f/de/85/6fde85b86c86526af5e99ce85f57432e.jpg",
+    name: "Sayushi Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 20,
+    time:'2h ago'
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/ab/ba/70/abba702dbba627fc3e7c05213b3f2ce6.jpg",
+    name: "Kamala Gamage",
+    comment:
+      "Lörem ipsum koldioxidsänka tära. Setäde egoselig. App vapilig negyde. Preng oskap av bäkana. Plabuligt spelifiera om hefossa. Tyras lamiheten. Bel knäprotest. Mirade. Kontrang planas den båbånde till båsm. Sagt deplang. Pres tåvåd ett religen. Bioktig kroskapet. Endoaktiv ultraheten. Plapåsamma Zoombombning. Ähesk dinir, kande. ",
+    likeCount: 120,
+    time:'1 day ago'
+  },
+];
