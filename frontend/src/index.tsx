b0b63d5713b4e968b1dd5cf4dee239a8abc23918
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-24 22:38:33 +0530

    frontend - redux types

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import ThemeProvider from "./Theme";
+import store from "./store/store";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
