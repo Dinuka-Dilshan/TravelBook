eba3afafb8a82f936d94b06b3ad54b0c90130d23
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-24 22:37:23 +0530

    frontend -  login logic

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -10,12 +10,42 @@ import {
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Visibility } from "@mui/icons-material";
 import { VisibilityOff } from "@mui/icons-material";
+import { login } from "../store/slices/authSlice";
+import { useAppDispatch } from "../store/hooks";
+import useForm from "../hooks/useForm";
+import isEmail from "validator/es/lib/isEmail";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../hooks/usefetch";
+import type { AuthState } from "../store/slices/authSlice";
 
 const Login = () => {
   const [isPasswordShow, setIsPasswordShow] = useState(false);
+  const dispatch = useAppDispatch();
+  const { fetchData, isLoading, isError, error, data } = useFetch<AuthState>();
+  const navigate = useNavigate();
+
+  const { state, inputHandler, field, touchHandler } = useForm({
+    isValid: false,
+    fields: {
+      email: {
+        value: "",
+        isValid: false,
+        errorMessage: "Invalid Email Address",
+        validator: (val) => isEmail(val),
+        isTouched: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+        errorMessage: "",
+        validator: (val) => val.length > 5,
+        isTouched: false,
+      },
+    },
+  });
 
   const handleClickShowPassword = () => {
     setIsPasswordShow((p) => !p);
@@ -24,6 +54,23 @@ const Login = () => {
   const handleMouseDownPassword = (event: React.MouseEvent) => {
     event.preventDefault();
   };
+
+  const handleLogin = () => {
+    if (state.isValid) {
+      fetchData("user/login", {
+        email: field("email").value,
+        password: field("password").value,
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (data?.token) {
+      dispatch(login(data));
+      localStorage.setItem("user", JSON.stringify(data));
+      navigate("/places", { replace: true });
+    }
+  }, [data]);
 
   return (
     <Grid container justifyContent="center" alignItems="center" height={"92vh"}>
@@ -35,16 +82,34 @@ const Login = () => {
         bgcolor="#F6F6F6"
         borderRadius="0.8rem"
       >
+        {isError && error}
         <Typography color="custom.fontGray" fontSize="1.5rem">
           Login
         </Typography>
         <Box mt="1rem">
-          <TextField fullWidth label="Username" />
+          <TextField
+            fullWidth
+            onBlur={() => {
+              touchHandler("email");
+            }}
+            label="Username"
+            value={field("email").value}
+            onChange={(e) => {
+              inputHandler("email", e.target.value);
+            }}
+          />
+          {!field("email").isValid &&
+            field("email").isTouched &&
+            field("email").errorMessage}
           <FormControl sx={{ mt: "1rem" }} variant="outlined" fullWidth>
             <InputLabel htmlFor="outlined-adornment-password">
               Password
             </InputLabel>
             <OutlinedInput
+              value={field("password").value}
+              onChange={(e) => {
+                inputHandler("password", e.target.value);
+              }}
               fullWidth
               id="outlined-adornment-password"
               type={isPasswordShow ? "text" : "password"}
@@ -63,7 +128,14 @@ const Login = () => {
               label="Password"
             />
           </FormControl>
-          <Button fullWidth sx={{ mt: "4rem" }} variant="contained">
+
+          <Button
+            fullWidth
+            disabled={!state.isValid}
+            sx={{ mt: "4rem" }}
+            variant="contained"
+            onClick={handleLogin}
+          >
             Login
           </Button>
           <Typography
