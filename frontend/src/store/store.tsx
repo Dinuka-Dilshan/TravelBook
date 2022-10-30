ec3fbe936205cc11c29ac2cecbf44dafb7f65691
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-30 18:00:11 +0530

    frontend - fix redux store

--- frontend/src/store/store.tsx
+++ frontend/src/store/store.tsx
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import notificationReducer from "./slices/notificationSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: { auth: authReducer, notification: notificationReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
