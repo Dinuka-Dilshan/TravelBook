c1b759fce8d000f84d71a5497a95b4268b8fffc3
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-31 22:04:42 +0530

    frontend - comment

---
+++ frontend/src/components/comment/index.tsx
@@ -0,0 +1,59 @@
+import {
+  FormControl,
+  InputAdornment,
+  OutlinedInput,
+  IconButton,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import useFetch from "../../hooks/useFetch";
+import { useEffect, useState } from "react";
+
+interface Props {
+  placeID: string;
+  refetch: () => void;
+}
+
+const Comment: React.FC<Props> = ({ placeID, refetch }) => {
+  const { isLoading, data, fetchData } = useFetch();
+  const [comment, setComment] = useState("");
+
+  const handleSend = () => {
+    if (comment.length < 1) return;
+    fetchData("place/comment", {
+      body: {
+        placeID,
+        content: comment,
+      },
+      method: "POST",
+      useToken: true,
+    });
+    setComment("");
+  };
+
+  useEffect(() => {
+    if (isLoading) return;
+    if (data) refetch();
+  }, [isLoading, data]);
+
+  return (
+    <FormControl sx={{ m: 1 }} variant="outlined" fullWidth>
+      <OutlinedInput
+        disabled={isLoading}
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        placeholder="Type your comment here..."
+        fullWidth
+        id="outlined-adornment-password"
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton edge="end" onClick={handleSend}>
+              <SendIcon sx={{ transform: "rotate(-35deg)" }} />
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+    </FormControl>
+  );
+};
+
+export default Comment;
