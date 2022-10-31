b06ddce991f67a2f8038209952fcfe934e66ead7
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-31 22:06:39 +0530

    frontend - message component fix

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -2,11 +2,11 @@ import { Avatar, Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 import { useState } from "react";
+import { formatDistanceToNow, parseISO } from "date-fns";
 
 const Message = ({ image, name, comment, likeCount, time }) => {
-  const [isLiked, setIsLiked] = useState(false);
-  const likeHandler = () => setIsLiked((p) => !p);
-
+  // const [isLiked, setIsLiked] = useState(false);
+  // const likeHandler = () => setIsLiked((p) => !p);
   return (
     <Box display="flex" justifyContent="space-around" p="1rem" mb="1rem">
       <Avatar src={image} />
@@ -16,20 +16,29 @@ const Message = ({ image, name, comment, likeCount, time }) => {
         mx="1rem"
         borderRadius="1rem"
         position="relative"
+        width={"100%"}
       >
         <Typography fontWeight="bold">{name}</Typography>
         <Typography mt="0.8rem">{comment}</Typography>
-        <Box position="absolute " bottom="-10%" display="flex" gap={5}>
-          <Typography>{time}</Typography>
+        <Box
+          position="absolute "
+          bottom="-25%"
+          display="flex"
+          gap={5}
+          p={"0.2rem"}
+        >
+          <Typography fontSize={"0.8rem"}>
+            {formatDistanceToNow(parseISO(time), { addSuffix: true })}
+          </Typography>
         </Box>
         <Box
           position="absolute"
-          bottom="-10%"
+          bottom="-25%"
           display="flex"
           gap={5}
           right="5%"
         >
-          <Box
+          {/* <Box
             width="fit-content"
             height="1.5rem"
             borderRadius="1.5rem"
@@ -54,7 +63,7 @@ const Message = ({ image, name, comment, likeCount, time }) => {
               />{" "}
               {likeCount}
             </Button>
-          </Box>
+          </Box> */}
         </Box>
       </Box>
     </Box>
