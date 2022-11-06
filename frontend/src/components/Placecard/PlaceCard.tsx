bbec22dddb38d3f155eebf9167886ec14b8e7e89
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-11-06 20:03:42 +0530

    frontend - upgrade place card

--- frontend/src/components/Placecard/PlaceCard.tsx
+++ frontend/src/components/Placecard/PlaceCard.tsx
@@ -1,3 +1,5 @@
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
 import {
   Button,
   Card,
@@ -9,16 +11,20 @@ import {
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
 import { useState } from "react";
-import ShareLocation from "../ShareLocation/ShareLocation";
 import { useNavigate } from "react-router-dom";
 import { Place } from "../../models/Place";
-import { capitalizeEachFirst } from "../../utils/string";
+import { capitalizeEachFirst, truncate } from "../../utils/string";
+import AvatarLink from "../Avatar";
+import ShareLocation from "../ShareLocation/ShareLocation";
 
-const PlaceCard: React.FC<Place> = ({ name, description, _id, photos }) => {
+const PlaceCard: React.FC<Place> = ({
+  name,
+  description,
+  _id,
+  photos,
+  addedBy,
+}) => {
   const [isShareOpen, setIsShareOpen] = useState(false);
   const navigate = useNavigate();
 
@@ -42,30 +48,42 @@ const PlaceCard: React.FC<Place> = ({ name, description, _id, photos }) => {
       )}
       <CardActionArea onClick={clickHandler}>
         <CardMedia component="img" alt="" height="220" src={photos[0]} />
-        <CardContent>
+        <CardContent sx={{ minHeight: "10rem" }}>
           <Box
             display="flex"
             justifyContent="space-between"
             alignItems="center"
           >
             <Typography fontSize="1.5rem" component="div">
-              {capitalizeEachFirst(name)}
+              {truncate(capitalizeEachFirst(name), 18)}
             </Typography>
             <Rating readOnly value={5} />
           </Box>
-          <Typography>{description}</Typography>
+          <Typography>{truncate(description, 250)}</Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions sx={{ justifyContent: "flex-end", pt: 0 }}>
-        <Button>
-          <LocationOnIcon />
-        </Button>
-        <Button>
-          <FavoriteIcon />
-        </Button>
-        <Button onClick={shareClickHandler}>
-          <ShareIcon />
-        </Button>
+      <CardActions
+        sx={{ justifyContent: "space-between", pt: 0, pl: "1rem" }}
+      >
+        <Box display="flex" alignItems="center">
+          <AvatarLink
+            size="30px"
+            image={addedBy.profilePicture}
+            userID={addedBy._id}
+            name={addedBy.name}
+          />
+          <Typography fontSize="0.9rem" fontWeight="bold" pl="0.5rem">
+            {addedBy.name}
+          </Typography>
+        </Box>
+        <Box>
+          <Button>
+            <FavoriteIcon />
+          </Button>
+          <Button onClick={shareClickHandler}>
+            <ShareIcon />
+          </Button>
+        </Box>
       </CardActions>
     </Card>
   );
