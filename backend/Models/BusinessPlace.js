845f2e3d9d0ff6f71b689886b970d8edf89fe3d5
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-11-13 12:48:10 +0530

    backend - business place modal

---
+++ backend/Models/BusinessPlace.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const placeSchema = new Schema({
+  name: String,
+  phoneNumber: string,
+  packages:[{
+
+  }],
+  description: String,
+  state: String,
+  country: String,
+  latitude: Number,
+  longitude: Number,
+  photos: [String],
+  addedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  addedOn: Date,
+  comments: [
+    {
+      author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      time: Date,
+      content: String,
+    },
+  ],
+  ratings: [
+    {
+      amount: Number,
+      user: String,
+    },
+  ],
+  viewRecords: [
+    {
+      time: Date,
+      user: { type: mongoose.Types.ObjectId, ref: "User" },
+    },
+  ],
+  likedBy: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+export default model("Place", placeSchema);
