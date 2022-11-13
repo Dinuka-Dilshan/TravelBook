f98123bfee4a81dab9e8c68963f70931e8fda7ab
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-11-13 12:45:46 +0530

    frontend - like

---
+++ frontend/src/components/Like/index.tsx
@@ -0,0 +1,60 @@
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { IconButton } from "@mui/material";
+import { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import { Place } from "../../models/Place";
+import { useAppSelector } from "../../store/hooks";
+import { selectUser } from "../../store/slices/authSlice";
+
+interface Props {
+  placeID: string;
+  isLiked?: boolean;
+  p?: string;
+}
+
+const Like: React.FC<Props> = ({ placeID, p }) => {
+  const { _id } = useAppSelector(selectUser);
+  const [isLiked, setIsLiked] = useState(false);
+  const { data, fetchData } = useFetch<Place[]>();
+  const { fetchData: likeOrUnlikePlace } = useFetch();
+
+  useEffect(() => {
+    const place = data?.filter((place) => place._id === placeID)[0];
+    if (place && _id) {
+      setIsLiked(place.likedBy.includes(_id));
+    }
+  }, [placeID, data, _id]);
+
+  useEffect(() => {
+    fetchData("place/likedPlaces", {
+      method: "GET",
+      type: "authenticated",
+    });
+  }, [fetchData, placeID]);
+
+  const likeToggleHandler = () => {
+    setIsLiked((prev) => {
+      if (prev) {
+        likeOrUnlikePlace(`place/${placeID}/unLike`, {
+          method: "POST",
+          type: "authenticated",
+        });
+      } else {
+        likeOrUnlikePlace(`place/${placeID}/like`, {
+          method: "POST",
+          type: "authenticated",
+        });
+      }
+
+      return !prev;
+    });
+  };
+
+  return (
+    <IconButton onClick={likeToggleHandler} sx={{ p }}>
+      <FavoriteIcon sx={{ color: isLiked ? "#FF5D7A" : "" }} />
+    </IconButton>
+  );
+};
+
+export default Like;
