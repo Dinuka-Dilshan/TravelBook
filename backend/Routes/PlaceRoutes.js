67b696e0b0fe39a5230710afa8edcc1b83dd4cf0
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-11-13 12:47:37 +0530

    backend - like unlike routes

--- backend/Routes/PlaceRoutes.js
+++ backend/Routes/PlaceRoutes.js
@@ -14,15 +14,23 @@ import {
   getAllPlaces,
   addPlacePhoto,
   getPlaceByID,
+  placeViewRecord,
+  getLikedPlacesController,
+  LikePlaceController,
+  unLikePlaceController,
 } from "../Controllers/Place/PlaceController.js";
 import { upload } from "../MiddleWare/FileParse.js";
 
 const router = express.Router();
 router.use(Auth);
 router.get("/", getAllPlaces);
+router.get("/likedPlaces", getLikedPlacesController);
 router.get("/:id", getPlaceByID);
 router.post("/", upload.single("placeImage"), placeValidation, addPlace);
 router.post("/comment", commentValidation, addComment);
+router.post("/:id/like", LikePlaceController);
+router.post("/:id/unlike", unLikePlaceController);
+router.post("/:id/view", placeViewRecord);
 router.post(
   "/addPhoto",
   upload.single("placeImage"),
