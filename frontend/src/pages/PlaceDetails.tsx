16dfc22ce585df9390da469d0fca0402a6fc516f
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-11-13 12:43:51 +0530

    frontend - place details new ui

--- frontend/src/pages/PlaceDetails.tsx
+++ frontend/src/pages/PlaceDetails.tsx
@@ -1,25 +1,27 @@
 import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import ShareIcon from "@mui/icons-material/Share";
 import StarIcon from "@mui/icons-material/Star";
+import { BsCalendar2Date } from "react-icons/bs";
 import {
   Container,
   Grid,
   IconButton,
   ImageList,
   ImageListItem,
+  LinearProgress,
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import { formatRelative, parseISO } from "date-fns";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import AddRating from "../components/AddRating";
 import AvatarLink from "../components/Avatar";
 import CommentSecton from "../components/CommentSection";
 import ErrorFS from "../components/ErrorFS";
+import Like from "../components/Like";
 import LoaderFS from "../components/LoaderFS";
 import ViewLocation from "../components/Location";
 import ImageUpload from "../components/PlaceImageUpload";
@@ -28,7 +30,7 @@ import ShareLocation from "../components/ShareLocation/ShareLocation";
 import useFetch from "../hooks/useFetch";
 import useHelmet from "../hooks/useHelmet";
 import { Place } from "../models/Place";
-import { capitalizeEachFirst } from "../utils/string";
+import { capitalizeEachFirst, removeWhiteSpacesWith } from "../utils/string";
 const srcset = (image: string, size: number, rows = 1, cols = 1) => {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
@@ -57,6 +59,7 @@ const PlaceDetails = () => {
   });
 
   const [refetch, setRefetch] = useState(false);
+  const { fetchData: notifyViewRecord } = useFetch();
   useHelmet(place?.name);
 
   const iconButtonClickhandler = (key: keyof typeof iconButtonStatus) => {
@@ -68,18 +71,36 @@ const PlaceDetails = () => {
     });
   };
 
-  const refetchPlaceDetails = () => setRefetch((p) => !p);
+  const refetchPlaceDetails = useCallback(() => setRefetch((p) => !p), []);
 
   useEffect(() => {
     fetchData(`place/${id}`, { method: "GET", type: "authenticated" });
   }, [refetch]);
 
+  useEffect(() => {
+    notifyViewRecord(`place/${id}/view`, {
+      method: "POST",
+      type: "authenticated",
+    });
+  }, []);
+
   if (isError) {
     return <ErrorFS error={error} />;
   }
 
+  if (isLoading && !place) {
+    return (
+      <LinearProgress
+        sx={{
+          background: "linear-gradient(to right, red, purple)",
+          height: "0.1rem",
+        }}
+      />
+    );
+  }
+
   return (
-    <Container sx={{ px: { lg: "4rem", xs: "1rem" }, pt: "1.5rem" }}>
+    <Container sx={{ px: { lg: "0", xs: "1rem" }, pt: "0" }}>
       {isLoading && <LoaderFS />}
       {iconButtonStatus.isShareOpen && (
         <ShareLocation
@@ -127,15 +148,37 @@ const PlaceDetails = () => {
           onSuccess={refetchPlaceDetails}
         />
       )}
-      <Grid container mb="2rem">
+      <Grid container mb="2rem" >
         <Grid item xs={12} lg={6}>
+          {place && (
+            <ImageList
+              sx={{ width: "100%", maxHeight: 500 }}
+              variant="quilted"
+              cols={4}
+              rowHeight={121}
+            >
+              {place.photos.map((photo, index) => (
+                <ImageListItem key={index} cols={4} rows={4}>
+                  <img {...srcset(photo, 121, 2, 1)} alt={""} loading="lazy" />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          )}
           <Box
             display="flex"
             justifyContent="space-between"
             alignItems="center"
+            pb={"1rem"}
+            border={"0.1rem solid #EFEFEF"}
+            borderTop={0}
+            borderLeft={0}
+            borderRight={0}
+            boxSizing={"border-box"}
+            mr={"0.5rem"}
           >
             <Typography
               fontWeight={"bold"}
+              fontFamily={"Poor Story, cursive"}
               sx={{ fontSize: { lg: "1.5rem", xs: "1.3rem" } }}
             >
               {capitalizeEachFirst(place?.name || "")}
@@ -156,15 +199,12 @@ const PlaceDetails = () => {
               >
                 <LocationOnIcon />
               </IconButton>
-              <IconButton
-                onClick={() => iconButtonClickhandler("isHeartClicked")}
-              >
-                <FavoriteIcon
-                  sx={{
-                    color: iconButtonStatus.isHeartClicked ? "#FF5D7A" : "",
-                  }}
-                />
-              </IconButton>
+
+              <Like
+                placeID={place?._id || ""}
+                isLiked={place?.isLiked || false}
+              />
+
               <IconButton onClick={() => iconButtonClickhandler("isRaingOpen")}>
                 <StarIcon />
               </IconButton>
@@ -173,65 +213,38 @@ const PlaceDetails = () => {
               </IconButton>
             </Box>
           </Box>
-          {place && (
-            <ImageList
-              sx={{ width: "100%", maxHeight: 500 }}
-              variant="quilted"
-              cols={4}
-              rowHeight={121}
-            >
-              {place.photos.map((photo, index) => (
-                <ImageListItem key={index} cols={4} rows={4}>
-                  <img {...srcset(photo, 121, 2, 1)} alt={""} loading="lazy" />
-                </ImageListItem>
-              ))}
-            </ImageList>
-          )}
+
+          <Typography letterSpacing={1.2} py={"1rem"}>
+            {place?.description}
+          </Typography>
           <PlaceLocationDetailsCard
             country={place?.country || ""}
             state={place?.state || ""}
             longitude={place?.longitude || 0}
             latitude={place?.latitude || 0}
           />
-          <Box
-            display="flex"
-            p={"1rem"}
-            mb="1rem"
-            bgcolor="#DEEFFF"
-            justifyContent={"space-between"}
-            borderRadius={"0.2rem"}
-          >
+          <Box display="flex" mb="1rem" justifyContent={"space-between"}>
             <Box display="flex" alignItems="center" gap={1}>
               <AvatarLink
                 image={place?.addedBy.profilePicture || ""}
                 userID={place?.addedBy._id || ""}
                 name={place?.addedBy.name || ""}
               />
-              <Typography fontWeight={"bold"} fontSize="0.9rem">
-                {place?.addedBy.name}
-              </Typography>
-            </Box>
-            <Box display="flex" alignItems="center" gap={1}>
-              <CalendarMonthIcon />
-              {place?.addedOn && (
-                <Typography fontSize="0.9rem">
-                  {formatRelative(parseISO(place.addedOn), new Date())}
+              <Box>
+                <Typography fontWeight={"bold"} fontSize="0.9rem">
+                  {removeWhiteSpacesWith(
+                    place?.addedBy.name?.toLowerCase() || "",
+                    "_"
+                  )}
                 </Typography>
-              )}
+                {place?.addedOn && (
+                  <Typography fontSize="0.8rem" color={"#CACACA"}>
+                    {formatRelative(parseISO(place.addedOn), new Date())}
+                  </Typography>
+                )}
+              </Box>
             </Box>
           </Box>
-          <Box display="flex" alignItems="center" gap={1} mb="1rem">
-            <StarIcon fontSize="small" />
-            <Typography fontSize="0.8rem">4.5</Typography>
-          </Box>
-
-          <Typography
-            bgcolor="custom.gray"
-            borderRadius={"0.2rem"}
-            p={"0.7rem"}
-          >
-            {place?.description}
-          </Typography>
         </Grid>
         <Grid item xs={12} lg={6} mt="2rem">
           {place?._id && <CommentSecton placeID={place._id} />}
