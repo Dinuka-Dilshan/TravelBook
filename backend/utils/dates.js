33e322b4b33871a1890ec43acc81a45aa068b10d
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2023-02-15 22:35:32 +0530

    backend - date utils

---
+++ backend/utils/dates.js
@@ -0,0 +1,27 @@
+export function dateDiffInDays(a, b) {
+  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+const monthNames = [
+  "",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const formatMonthlyReport = (report) =>
+  report.map((monthly) => ({ month: monthNames[monthly._id], ...monthly }));
