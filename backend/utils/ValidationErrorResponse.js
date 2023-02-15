a0988b195a64ef2ca5144248c9c1870a9e4c111f
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2023-02-15 22:36:23 +0530

    backend - validation response fix

--- backend/utils/ValidationErrorResponse.js
+++ backend/utils/ValidationErrorResponse.js
@@ -1,11 +1,15 @@
 import { validationResult } from "express-validator";
+import ErrorResponse, { getErrorMessages } from "./ErrorResponse.js";
 
-export default (req, next) => {
+export default (req) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return next(
-      ErrorResponse({ code: 406, message: getErrorMessages(errors.array()) })
-    );
+    return ErrorResponse({
+      code: 406,
+      message: getErrorMessages(errors.array()),
+    });
+  } else {
+    return null;
   }
 };
